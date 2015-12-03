2406907a8512c400786239c6f8a9df1ad3ae9b49
alexis ardila <alexis.sdvsf701@gmail.com>
2015-12-03 12:40:39 -0500

    Esta es la clase principal que inicia todo el programa

---
+++ src/Clases/Principal.java
@@ -0,0 +1,16 @@
+package Clases;
+//Esta es la clase principal que Inicia Todo
+import Clases.frmSplash;
+import formularios.form_login;
+
+public class Principal {
+	
+
+	public static void main(String[] args) {
+		Datos misDatos = new Datos();
+		form_login miLogin = new form_login();
+		miLogin.setLocationRelativeTo(null);
+		miLogin.setDatos(misDatos);
+		miLogin.setVisible(true);
+	}	
+}
